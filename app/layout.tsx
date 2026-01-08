9e0be47161a32e9c02ba6a60fb436883d307c8cd
Alex Boulogne <alexboulogne@Alexs-Laptop.attlocal.net>
2026-01-08 10:57:00 -0500

    Add Google Analytics tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,6 +46,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-EHV72V19PV"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-EHV72V19PV');
+            `,
+          }}
+        />
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" sizes="any" />
       </head>
